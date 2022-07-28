366cc0edc4129521cdc34907783cdbc69613434d
yunhyunsung <gustjd5624@naver.com>
2022-07-28 10:57:30 +0900

    세 번째 add

--- Test1/src/test/main/MainClass.java
+++ Test1/src/test/main/MainClass.java
@@ -4,6 +4,6 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("와우 피곤피곤");
 		System.out.println("!!!");
-		
+		System.out.println("git added 하나 더");
 	}
 }
