b911e7300258d20b7709ef9eafd5f53216834159
yunhyunsung <gustjd5624@naver.com>
2022-07-29 09:35:06 +0900

    연습

--- Test1/src/test/main/MainClass.java
+++ Test1/src/test/main/MainClass.java
@@ -6,5 +6,8 @@ public class MainClass {
 		System.out.println("!!!");
 		System.out.println("git added 하나 더");
 		System.out.println("lab1 에서 완성함");
+		System.out.println("git 공부");
+		System.out.println("오늘은 7월29일");
+		
 	}
 }
